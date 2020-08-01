3269b7bacfa5ba9bc6216c3ce2b3e7558c9c4a77
Arun Mathew <arunvmathew95@gmail.com>
2020-08-02 00:25:35 +0530

    Removed unnecessary code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -34,28 +34,3 @@ func main() {
 
 	g.MainLoop()
 }
-
-// func main() {
-
-// 	conn, err := net.Dial("tcp", ":8888")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	message := fmt.Sprintln("/connect>John")
-
-// 	conn.Write([]byte(message))
-// 	message = fmt.Sprintln("/message>Hello world")
-// 	conn.Write([]byte(message))
-
-// 	b := make([]byte, len("Hello world"))
-// 	_, err = conn.Read(b)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println(string(b))
-
-// 	message = fmt.Sprintln("/disconnect>John")
-// 	conn.Write([]byte(message))
-// 	<-time.After(time.Second * 15)
-// }
